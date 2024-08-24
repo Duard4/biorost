f32dda4bab343f501a446bd3e5b873814c85e5cb
Eduard <parhomenkoeduard75@gmail.com>
2024-08-24 20:50:29 +0300

    some

--- src/js/data.js
+++ src/js/data.js
@@ -1,8 +1,8 @@
 // src/js/data.js
 
 export const items = [
-    { id: 0, type: 'org', frontImage: '../assets/products/12.png', backImage: '../assets/products/13.png', title: 'Біорост Універсал', description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut cum magnam reiciendis' },
-    { id: 1, type: 'org', frontImage: '../assets/products/14.png', backImage: '../assets/products/15.png', title: 'Біорост Старт', description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut cum magnam reiciendis' },
+    { id: 0, type: 'org', frontImage: '../assets/products/12.png', backImage: '../assets/products/13.jpg', title: 'Біорост Універсал', description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut cum magnam reiciendis' },
+    { id: 1, type: 'org', frontImage: '../assets/products/14.png', backImage: '../assets/products/15.jpg', title: 'Біорост Старт', description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut cum magnam reiciendis' },
     { id: 2, type: 'org', frontImage: '../assets/products/36.png', backImage: '../assets/products/37.png', title: 'Зима', description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut cum magnam reiciendis' },
     { id: 3, type: 'org', frontImage: '../assets/products/32.png', backImage: '../assets/products/33.png', title: 'Ультра Бор Азот', description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut cum magnam reiciendis' },
     { id: 4, type: 'org', frontImage: '../assets/products/34.png', backImage: '../assets/products/35.png', title: 'Бор+', description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut cum magnam reiciendis' },
@@ -10,15 +10,15 @@ export const items = [
     { id: 6, type: 'org', frontImage: '../assets/products/26.png', backImage: '../assets/products/27.png', title: 'Цинк+', description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut cum magnam reiciendis' },
     { id: 7, type: 'org', frontImage: '../assets/products/28.png', backImage: '../assets/products/29.png', title: 'Магній+', description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut cum magnam reiciendis' },
     { id: 8, type: 'org', frontImage: '../assets/products/38.png', backImage: '../assets/products/39.png', title: 'Суміш для наливу', description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut cum magnam reiciendis' },
-    { id: 9, type: 'org', frontImage: '../assets/products/6.png', backImage: '../assets/products/7.png', title: 'Гранула "Біорост"', description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut cum magnam reiciendis' },
-    { id: 10, type: 'ino', frontImage: '../assets/products/8.png',  backImage: '../assets/products/9.png',  title: 'Сухий інокулянт', description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut cum magnam reiciendis' },
+    { id: 9, type: 'org', frontImage: '../assets/products/6.png', backImage: '../assets/products/7.jpg', title: 'Гранула "Біорост"', description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut cum magnam reiciendis' },
+    { id: 10, type: 'ino', frontImage: '../assets/products/8.png',  backImage: '../assets/products/9.jpg',  title: 'Сухий інокулянт', description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut cum magnam reiciendis' },
     { id: 11, type: 'fun ins', frontImage: '../assets/products/22.png',  backImage: '../assets/products/23.png', title: 'Гаупсин', description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut cum magnam reiciendis' },
-    { id: 12, type: 'fun', frontImage: '../assets/products/16.png',  backImage: '../assets/products/17.png', title: 'Триходермін', description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut cum magnam reiciendis' },
+    { id: 12, type: 'fun', frontImage: '../assets/products/16.png',  backImage: '../assets/products/17.jpg', title: 'Триходермін', description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut cum magnam reiciendis' },
     { id: 13, type: 'fun', frontImage: '../assets/products/40.png',  backImage: '../assets/products/41.png', title: 'Гліокладин', description: 'Lorem ddddddddddddddddddddddddddddddddddipsum dolor sit amet consectetur adipisicing elit. Ut cum magnam reiciendis' },
     { id: 14, type: 'ins', frontImage: '../assets/products/18.png',  backImage: '../assets/products/19.png', title: 'Метаризин', description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut cum magnam reiciendis' },
     { id: 15, type: 'ins', frontImage: '../assets/products/20.png',  backImage: '../assets/products/21.png', title: 'Бітоксибацилін', description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut cum magnam reiciendis' },
     // { id: 16, type: 'ins', frontImage: '../assets/products/36.png',  backImage: '../assets/products/37.png', title: 'Квартоцид', description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut cum magnam reiciendis' },
-    { id: 17, type: 'bact', frontImage: '../assets/products/4.png',  backImage: '../assets/products/5.png', title: 'Біорост Бактерос', description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut cum magnam reiciendis' },
+    { id: 17, type: 'bact', frontImage: '../assets/products/4.png',  backImage: '../assets/products/5.jpg', title: 'Біорост Бактерос', description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut cum magnam reiciendis' },
     { id: 18, type: 'fito', frontImage: '../assets/products/24.png',  backImage: '../assets/products/25.png', title: 'Біорост Ауксин', description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut cum magnam reiciendis' },
     { id: 19, type: 'fito', frontImage: '../assets/products/10.png',  backImage: '../assets/products/11.png', title: 'Біорост Імунітет', description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut cum magnam reiciendis' },
     // { id: 20, type: 'comp', frontImage: '../assets/products/44.png',  backImage: '../assets/products/45.png', title: 'Біорост Симбіоз-Бар-Фоліар', description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut cum magnam reiciendis' }
