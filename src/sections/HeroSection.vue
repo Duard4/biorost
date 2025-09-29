716590537df0968497564fa00bb3df25bddb32fd
Duard4 <parhomenkoeduard75@gmail.com>
2025-09-29 21:27:09 +0300

    fix

--- src/sections/HeroSection.vue
+++ src/sections/HeroSection.vue
@@ -2,7 +2,7 @@
 <template>
   <section class="section hero">
     <div class="container">
-      <h1 class="hero-title">БІОРОСТ</h1>
+      <h1 class="hero-title">ТМ "БІОРОСТ"</h1>
       <p class="hero-text">
         Наша місія – забезпечити відновлення нашої годувальниці Землі
       </p>
